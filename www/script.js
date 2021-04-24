09d522771bb701eca34f94d9c64f836e3d1a6d19
Vasanth.V <imvasanthv@gmail.com>
2021-04-24 23:15:56 +0530

    Name changes

--- www/script.js
+++ www/script.js
@@ -48,11 +48,11 @@ const App = new Vue({
 	computed: {},
 	methods: {
 		audioToggle: function() {
-			local_media_stream.getAudioTracks()[0].enabled = !local_media_stream.getAudioTracks()[0].enabled;
+			localMediaStream.getAudioTracks()[0].enabled = !localMediaStream.getAudioTracks()[0].enabled;
 			this.audioEnabled = !this.audioEnabled;
 		},
 		videoToggle: function() {
-			local_media_stream.getVideoTracks()[0].enabled = !local_media_stream.getVideoTracks()[0].enabled;
+			localMediaStream.getVideoTracks()[0].enabled = !localMediaStream.getVideoTracks()[0].enabled;
 			this.videoEnabled = !this.videoEnabled;
 		},
 		screenShareToggle: function() {
@@ -79,8 +79,8 @@ const App = new Vue({
 						sender.replaceTrack(screenStream.getVideoTracks()[0]);
 					}
 					screenStream.getVideoTracks()[0].enabled = true;
-					const newStream = new MediaStream([screenStream.getVideoTracks()[0], local_media_stream.getAudioTracks()[0]]);
-					local_media_stream = newStream;
+					const newStream = new MediaStream([screenStream.getVideoTracks()[0], localMediaStream.getAudioTracks()[0]]);
+					localMediaStream = newStream;
 					attachMediaStream(document.getElementById("selfVideo"), newStream);
 					document.querySelector("#videos .video #selfVideo").classList.toggle("mirror");
 
@@ -99,32 +99,25 @@ const App = new Vue({
 const USE_AUDIO = true;
 const USE_VIDEO = true;
 
-let signaling_socket = null; /* our socket.io connection to our webserver */
-let local_media_stream = null; /* our own microphone / webcam */
+let signalingSocket = null; /* our socket.io connection to our webserver */
+let localMediaStream = null; /* our own microphone / webcam */
 let peers = {}; /* keep track of our peer connections, indexed by peer_id (aka socket.io id) */
-let peer_media_elements = {}; /* keep track of our <video>/<audio> tags, indexed by peer_id */
+let peerMediaElements = {}; /* keep track of our <video>/<audio> tags, indexed by peer_id */
 
 function init() {
-	// console.log("Connecting to signaling server");
-	signaling_socket = io(APP_URL);
-	signaling_socket = io();
+	signalingSocket = io(APP_URL);
+	signalingSocket = io();
 
-	signaling_socket.on("connect", function() {
-		// console.log("Connected to signaling server");
-		if (local_media_stream) join_chat_channel(ROOM_ID, {});
+	signalingSocket.on("connect", function() {
+		if (localMediaStream) join_chat_channel(ROOM_ID, {});
 		else
-			setup_local_media(function() {
-				/* once the user has given us access to their
-				 * microphone/camcorder, join the channel and start peering up */
+			setupLocalMedia(function() {
 				join_chat_channel(ROOM_ID, {});
 			});
 	});
-	signaling_socket.on("disconnect", function() {
-		// console.log("Disconnected from signaling server");
-		/* Tear down all of our peer connections and remove all the
-		 * media divs when we disconnect */
-		for (let peer_id in peer_media_elements) {
-			document.getElementById("videos").removeChild(peer_media_elements[peer_id].parentNode);
+	signalingSocket.on("disconnect", function() {
+		for (let peer_id in peerMediaElements) {
+			document.getElementById("videos").removeChild(peerMediaElements[peer_id].parentNode);
 			resizeVideos();
 		}
 		for (let peer_id in peers) {
@@ -132,36 +125,26 @@ function init() {
 		}
 
 		peers = {};
-		peer_media_elements = {};
+		peerMediaElements = {};
 	});
 
 	function join_chat_channel(channel, userdata) {
-		signaling_socket.emit("join", { channel: channel, userdata: userdata });
+		signalingSocket.emit("join", { channel: channel, userdata: userdata });
 	}
 
-	/**
-	 * When we join a group, our signaling server will send out 'addPeer' events to each pair
-	 * of users in the group (creating a fully-connected graph of users, ie if there are 6 people
-	 * in the room you will connect directly to the other 5, so there will be a total of 15
-	 * connections in the network).
-	 */
-	signaling_socket.on("addPeer", function(config) {
-		// console.log('Signaling server said to add peer:', config);
+	signalingSocket.on("addPeer", function(config) {
 		const peer_id = config.peer_id;
-		if (peer_id in peers) {
-			/* This could happen if the user joins multiple rooms where the other peer is also in. */
-			// console.log("Already connected to peer ", peer_id);
-			return;
-		}
-		const peer_connection = new RTCPeerConnection(
-			{ iceServers: ICE_SERVERS },
-			{ optional: [{ DtlsSrtpKeyAgreement: true }] } // this will no longer be needed by chrome eventually (supposedly), but is necessary for now to get firefox to talk to chrome
-		);
-		peers[peer_id] = peer_connection;
+		if (peer_id in peers) return;
 
-		peer_connection.onicecandidate = function(event) {
+		const peerConnection = new RTCPeerConnection(
+			{ iceServers: ICE_SERVERS },
+			{ optional: [{ DtlsSrtpKeyAgreement: true }] }
+		);
+		peers[peer_id] = peerConnection;
+
+		peerConnection.onicecandidate = function(event) {
 			if (event.candidate) {
-				signaling_socket.emit("relayICECandidate", {
+				signalingSocket.emit("relayICECandidate", {
 					peer_id: peer_id,
 					ice_candidate: {
 						sdpMLineIndex: event.candidate.sdpMLineIndex,
@@ -170,172 +153,88 @@ function init() {
 				});
 			}
 		};
-		peer_connection.onaddstream = function(event) {
-			// console.log("onAddStream", event);
-			// const videoWrap = document.createElement("div");
-			// videoWrap.className = "video";
-			const remote_media = getVideoElement();
-			// videoWrap.appendChild(remote_media);
-			// remote_media.setAttribute("playsinline", true);
-			// remote_media.mediaGroup = "remotevideo";
-			// remote_media.autoplay = true;
-			// remote_media.controls = false;
-			peer_media_elements[peer_id] = remote_media;
-			// document.getElementById("videos").appendChild(videoWrap);
-
-			// if (remote_media.requestFullscreen) {
-			// 	const fullScreenBtn = document.createElement("button");
-			// 	fullScreenBtn.className = "fullscreenbtn fas fa-expand";
-			// 	fullScreenBtn.addEventListener("click", (e) => {
-			// 		remote_media.requestFullscreen();
-			// 	});
-			// 	videoWrap.appendChild(fullScreenBtn);
-			// }
-
-			attachMediaStream(remote_media, event.stream);
+		peerConnection.onaddstream = function(event) {
+			const remoteMedia = getVideoElement();
+			peerMediaElements[peer_id] = remoteMedia;
+			attachMediaStream(remoteMedia, event.stream);
 			resizeVideos();
 		};
 
 		/* Add our local stream */
-		peer_connection.addStream(local_media_stream);
+		peerConnection.addStream(localMediaStream);
 
-		/* Only one side of the peer connection should create the
-		 * offer, the signaling server picks one to be the offerer.
-		 * The other user will get a 'sessionDescription' event and will
-		 * create an offer, then send back an answer 'sessionDescription' to us
-		 */
 		if (config.should_create_offer) {
-			// console.log("Creating RTC offer to ", peer_id);
-			peer_connection.createOffer(
-				function(local_description) {
-					// console.log("Local offer description is: ", local_description);
-					peer_connection.setLocalDescription(
-						local_description,
-						function() {
-							signaling_socket.emit("relaySessionDescription", {
+			peerConnection.createOffer(
+				(localDescription) => {
+					peerConnection.setLocalDescription(
+						localDescription,
+						() => {
+							signalingSocket.emit("relaySessionDescription", {
 								peer_id: peer_id,
-								session_description: local_description,
+								session_description: localDescription,
 							});
-							// console.log("Offer setLocalDescription succeeded");
 						},
-						function() {
-							alert("Offer setLocalDescription failed!");
-						}
+						() => alert("Offer setLocalDescription failed!")
 					);
 				},
-				function(error) {
-					console.log("Error sending offer: ", error);
-				}
+				(error) => console.log("Error sending offer: ", error)
 			);
 		}
 	});
 
-	/**
-	 * Peers exchange session descriptions which contains information
-	 * about their audio / video settings and that sort of stuff. First
-	 * the 'offerer' sends a description to the 'answerer' (with type
-	 * "offer"), then the answerer sends one back (with type "answer").
-	 */
-	signaling_socket.on("sessionDescription", function(config) {
-		// console.log('Remote description received: ', config);
+	signalingSocket.on("sessionDescription", function(config) {
 		const peer_id = config.peer_id;
 		const peer = peers[peer_id];
-		const remote_description = config.session_description;
-		// console.log(config.session_description);
+		const remoteDescription = config.session_description;
 
-		const desc = new RTCSessionDescription(remote_description);
+		const desc = new RTCSessionDescription(remoteDescription);
 		peer.setRemoteDescription(
 			desc,
-			function() {
-				// console.log("setRemoteDescription succeeded");
-				if (remote_description.type == "offer") {
-					// console.log("Creating answer");
+			() => {
+				if (remoteDescription.type == "offer") {
 					peer.createAnswer(
-						function(local_description) {
-							// console.log("Answer description is: ", local_description);
+						(localDescription) => {
 							peer.setLocalDescription(
-								local_description,
-								function() {
-									signaling_socket.emit("relaySessionDescription", {
+								localDescription,
+								() => {
+									signalingSocket.emit("relaySessionDescription", {
 										peer_id: peer_id,
-										session_description: local_description,
+										session_description: localDescription,
 									});
-									// console.log("Answer setLocalDescription succeeded");
 								},
-								function() {
-									alert("Answer setLocalDescription failed!");
-								}
+								() => alert("Answer setLocalDescription failed!")
 							);
 						},
-						function(error) {
-							console.log("Error creating answer: ", error);
-							// console.log(peer);
-						}
+						(error) => console.log("Error creating answer: ", error)
 					);
 				}
 			},
-			function(error) {
-				console.log("setRemoteDescription error: ", error);
-			}
+			(error) => console.log("setRemoteDescription error: ", error)
 		);
-		// console.log("Description Object: ", desc);
 	});
 
-	/**
-	 * The offerer will send a number of ICE Candidate blobs to the answerer so they
-	 * can begin trying to find the best path to one another on the net.
-	 */
-	signaling_socket.on("iceCandidate", function(config) {
+	signalingSocket.on("iceCandidate", function(config) {
 		const peer = peers[config.peer_id];
-		const ice_candidate = config.ice_candidate;
-		peer.addIceCandidate(new RTCIceCandidate(ice_candidate));
+		const iceCandidate = config.ice_candidate;
+		peer.addIceCandidate(new RTCIceCandidate(iceCandidate));
 	});
 
-	/**
-	 * When a user leaves a channel (or is disconnected from the
-	 * signaling server) everyone will recieve a 'removePeer' message
-	 * telling them to trash the media channels they have open for those
-	 * that peer. If it was this client that left a channel, they'll also
-	 * receive the removePeers. If this client was disconnected, they
-	 * wont receive removePeers, but rather the
-	 * signaling_socket.on('disconnect') code will kick in and tear down
-	 * all the peer sessions.
-	 */
-	signaling_socket.on("removePeer", function(config) {
-		// console.log('Signaling server said to remove peer:', config);
+	signalingSocket.on("removePeer", function(config) {
 		const peer_id = config.peer_id;
-		if (peer_id in peer_media_elements) {
-			document.getElementById("videos").removeChild(peer_media_elements[peer_id].parentNode);
+		if (peer_id in peerMediaElements) {
+			document.getElementById("videos").removeChild(peerMediaElements[peer_id].parentNode);
 			resizeVideos();
 		}
 		if (peer_id in peers) {
 			peers[peer_id].close();
 		}
-
 		delete peers[peer_id];
-		delete peer_media_elements[config.peer_id];
+		delete peerMediaElements[config.peer_id];
 	});
-	// document.getElementById("roomurl").textContent = APP_URL + "/" + ROOM_ID;
-	// document.getElementById("roomurl").addEventListener("click", (event) => {
-	// 	let range, selection;
-	// 	selection = window.getSelection();
-	// 	range = document.createRange();
-	// 	range.selectNodeContents(event.target);
-	// 	selection.removeAllRanges();
-	// 	selection.addRange(range);
-	// });
-	// document.getElementById("closebtn").addEventListener("click", () => {
-	// 	document.getElementById("intro").style.display = "none";
-	// });
 }
-const attachMediaStream = function(element, stream) {
-	// console.log('DEPRECATED, attachMediaStream will soon be removed.');
-	element.srcObject = stream;
-};
-
-function setup_local_media(callback, errorback) {
-	if (local_media_stream != null) {
-		/* ie, if we've already been initialized */
+const attachMediaStream = (element, stream) => (element.srcObject = stream);
+function setupLocalMedia(callback, errorback) {
+	if (localMediaStream != null) {
 		if (callback) callback();
 		return;
 	}
@@ -343,45 +242,9 @@ function setup_local_media(callback, errorback) {
 	navigator.mediaDevices
 		.getUserMedia({ audio: USE_AUDIO, video: USE_VIDEO })
 		.then((stream) => {
-			local_media_stream = stream;
-			// const videoWrap = document.createElement("div");
-			// videoWrap.className = "video";
-			// videoWrap.setAttribute("id", "selfVideoWrap");
-
-			// const btnWrap = document.createElement("div");
-			// btnWrap.setAttribute("id", "btnWrap");
-			// const muteBtn = document.createElement("button");
-			// muteBtn.setAttribute("id", "mutebtn");
-			// muteBtn.className = "fas fa-microphone";
-			// muteBtn.addEventListener("click", (e) => {
-			// 	local_media_stream.getAudioTracks()[0].enabled = !local_media_stream.getAudioTracks()[0].enabled;
-			// 	e.target.className = "fas fa-microphone" + (local_media_stream.getAudioTracks()[0].enabled ? "" : "-slash");
-			// });
-			// btnWrap.appendChild(muteBtn);
-
-			// const videoMuteBtn = document.createElement("button");
-			// videoMuteBtn.setAttribute("id", "videomutebtn");
-			// videoMuteBtn.className = "fas fa-video";
-			// videoMuteBtn.addEventListener("click", (e) => {
-			// 	local_media_stream.getVideoTracks()[0].enabled = !local_media_stream.getVideoTracks()[0].enabled;
-			// 	e.target.className = "fas fa-video" + (local_media_stream.getVideoTracks()[0].enabled ? "" : "-slash");
-			// });
-			// btnWrap.appendChild(videoMuteBtn);
-
-			// videoWrap.appendChild(btnWrap); // append all buttons to the local video wrap
-
-			const local_media = getVideoElement(true);
-
-			// local_media.setAttribute("id", "selfVideo");
-			// local_media.setAttribute("playsinline", true);
-			// local_media.className = "mirror";
-			// local_media.autoplay = true;
-			// local_media.muted = true;
-			// local_media.volume = 0;
-			// local_media.controls = false;
-			// videoWrap.appendChild(local_media);
-			// document.getElementById("videos").appendChild(videoWrap);
-			attachMediaStream(local_media, stream);
+			localMediaStream = stream;
+			const localMedia = getVideoElement(true);
+			attachMediaStream(localMedia, stream);
 			resizeVideos();
 			if (callback) callback();
 		})
@@ -410,22 +273,6 @@ const getVideoElement = (isLocal) => {
 	videoWrap.appendChild(media);
 	document.getElementById("videos").appendChild(videoWrap);
 	return media;
-
-	// const local_media = document.createElement("video");
-	// local_media.setAttribute("id", "selfVideo");
-	// local_media.setAttribute("playsinline", true);
-	// local_media.className = "mirror";
-	// local_media.autoplay = true;
-	// local_media.muted = true;
-	// local_media.volume = 0;
-	// local_media.controls = false;
-
-	// const remote_media = document.createElement("video");
-	// videoWrap.appendChild(remote_media);
-	// remote_media.setAttribute("playsinline", true);
-	// remote_media.mediaGroup = "remotevideo";
-	// remote_media.autoplay = true;
-	// remote_media.controls = false;
 };
 
 const resizeVideos = () => {
