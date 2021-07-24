ef330e1bbca5ab859f033fc1a104397540a13e64
Tomer <tomer@shvueli.com>
2021-07-24 12:29:43 -0400

    feat: Import a separate file for ICE servers

--- www/script.js
+++ www/script.js
@@ -1,23 +1,4 @@
 /* globals App, io, cabin*/
-const ICE_SERVERS = [
-	{ urls: "stun:stun.l.google.com:19302" },
-	{ urls: "stun:stun.stunprotocol.org:3478" },
-	{ urls: "stun:stun.sipnet.net:3478" },
-	{ urls: "stun:stun.ideasip.com:3478" },
-	{ urls: "stun:stun.iptel.org:3478" },
-	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
-	{
-		urls: [
-			"turn:173.194.72.127:19305?transport=udp",
-			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-			"turn:173.194.72.127:443?transport=tcp",
-			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
-		],
-		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
-		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
-	},
-];
-
 const APP_URL = (() => {
 	const protocol = "http" + (location.hostname == "localhost" ? "" : "s") + "://";
 	return protocol + location.hostname + (location.hostname == "localhost" ? ":3000" : "");
