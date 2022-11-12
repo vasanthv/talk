50644b41fcc3e050d5ad9e5b51e3e50974aa9386
vasanth <imvasanthv@gmail.com>
2022-11-11 21:14:09 -0500

    Fixed GA script

--- postinstall.js
+++ postinstall.js
@@ -2,12 +2,13 @@
 
 const fs = require("fs");
 const path = require("path");
-if (process.env.NODE_ENV === "production" && process.env.TALK_GA_SCRIPT) {
+if (process.env.NODE_ENV === "production" && process.env.GA_TAG_ID) {
+	console.log("Inserting GA script tag");
 	const serviceWorkerContents = fs.readFileSync(path.join(__dirname, "www/index.html")).toString();
 
 	const newServiceWorkerContents = serviceWorkerContents.replace(
 		"<!-- GOOGLE_ANALYTICS_SCRIPT -->",
-		process.env.TALK_GA_SCRIPT
+		`<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TAG_ID}"></script><script>window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "${process.env.GA_TAG_ID}");</script>`
 	);
 
 	fs.writeFileSync(path.join(__dirname, "www/index.html"), newServiceWorkerContents);
