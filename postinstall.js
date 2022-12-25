1ed646c569d280adcff65032d60a16a99f658e95
vasanth <imvasanthv@gmail.com>
2022-12-25 07:37:25 -0500

    Fixed legal page & changed logo color

--- postinstall.js
+++ postinstall.js
@@ -4,12 +4,13 @@ const fs = require("fs");
 const path = require("path");
 if (process.env.NODE_ENV === "production" && process.env.GA_TAG_ID) {
 	console.log("Inserting GA script tag");
-	const serviceWorkerContents = fs.readFileSync(path.join(__dirname, "www/index.html")).toString();
+	const scriptTag = `<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TAG_ID}"></script><script>window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "${process.env.GA_TAG_ID}");</script>`;
 
-	const newServiceWorkerContents = serviceWorkerContents.replace(
-		"<!-- GOOGLE_ANALYTICS_SCRIPT -->",
-		`<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TAG_ID}"></script><script>window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "${process.env.GA_TAG_ID}");</script>`
-	);
+	const indexFileContents = fs.readFileSync(path.join(__dirname, "www/index.html")).toString();
+	const newIndexFileContents = indexFileContents.replace("<!-- GOOGLE_ANALYTICS_SCRIPT -->", scriptTag);
+	fs.writeFileSync(path.join(__dirname, "www/index.html"), newIndexFileContents);
 
-	fs.writeFileSync(path.join(__dirname, "www/index.html"), newServiceWorkerContents);
+	const legalFileContents = fs.readFileSync(path.join(__dirname, "www/legal.html")).toString();
+	const newLegalFileContents = legalFileContents.replace("<!-- GOOGLE_ANALYTICS_SCRIPT -->", scriptTag);
+	fs.writeFileSync(path.join(__dirname, "www/legal.html"), newLegalFileContents);
 }
