d4bea2b772dd08a876c2d2a4373dd3a5fa31703f
vasanth <imvasanthv@gmail.com>
2022-12-26 13:15:15 -0500

    Moved from Google analytics to Cabin
    Public dashboard https://withcabin.com/public/5kEYu6KxQ7Kp

--- postinstall.js
+++ postinstall.js
@@ -2,15 +2,16 @@
 
 const fs = require("fs");
 const path = require("path");
-if (process.env.NODE_ENV === "production" && process.env.GA_TAG_ID) {
-	console.log("Inserting GA script tag");
-	const scriptTag = `<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TAG_ID}"></script><script>window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "${process.env.GA_TAG_ID}");</script>`;
+if (process.env.NODE_ENV === "production" && process.env.ANALYTICS_SCRIPT) {
+	console.log("Inserting Analytics script tag");
+
+	const scriptTag = `<script async defer src="${process.env.ANALYTICS_SCRIPT}"></script>`;
 
 	const indexFileContents = fs.readFileSync(path.join(__dirname, "www/index.html")).toString();
-	const newIndexFileContents = indexFileContents.replace("<!-- GOOGLE_ANALYTICS_SCRIPT -->", scriptTag);
+	const newIndexFileContents = indexFileContents.replace("<!-- ANALYTICS_SCRIPT -->", scriptTag);
 	fs.writeFileSync(path.join(__dirname, "www/index.html"), newIndexFileContents);
 
 	const legalFileContents = fs.readFileSync(path.join(__dirname, "www/legal.html")).toString();
-	const newLegalFileContents = legalFileContents.replace("<!-- GOOGLE_ANALYTICS_SCRIPT -->", scriptTag);
+	const newLegalFileContents = legalFileContents.replace("<!-- ANALYTICS_SCRIPT -->", scriptTag);
 	fs.writeFileSync(path.join(__dirname, "www/legal.html"), newLegalFileContents);
 }
