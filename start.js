4d6994c5f1ecd227844290980a0fd0bd47f203aa
vasanth <imvasanthv@gmail.com>
2023-06-27 18:23:11 -0400

    Allow CORS

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const path = require("path");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const signallingServer = require("./server/signalling-server.js");
 
